ba681d8c6dee6a1085806ed08b50d7fa8cb8c0f1
Alexander <aocsa.cs@gmail.com>
2021-05-21 12:08:34 -0500

    CUDA support in the CSR layout: CPU  addmm Fix
    ghstack-source-id: 6defd3a79209b2668c47e772248bd03331b38350
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58622
    minor format fix
    
    
    minor mkl_test fix
    
    
    minor fix
    run ci test
    implements matvec using MKL

--- aten/src/ATen/native/mkl/SparseCsrLinearAlgebra.cpp
+++ aten/src/ATen/native/mkl/SparseCsrLinearAlgebra.cpp
@@ -105,22 +105,35 @@ class SparseCsrMKLInterface {
       float* dense,
       float alpha,
       float beta,
-      MKL_INT nrows,
-      MKL_INT ncols,
-      MKL_INT dense_ncols) {
-    int stat = mkl_sparse_s_mm(
+      MKL_INT M,
+      MKL_INT K,
+      MKL_INT N) {
+    int stat;
+    if (N == 1) {
+      stat = mkl_sparse_s_mv(
+        SPARSE_OPERATION_NON_TRANSPOSE,
+        alpha,
+        A,
+        desc,
+        dense,
+        beta,
+        res);
+      TORCH_CHECK(stat == 0, "mkl_sparse_s_mv failed with error code: ", stat);
+    } else {
+      stat = mkl_sparse_s_mm(
         SPARSE_OPERATION_NON_TRANSPOSE,
         alpha,
         A,
         desc,
         SPARSE_LAYOUT_ROW_MAJOR,
         dense,
-        dense_ncols,
-        dense_ncols,
+        N,
+        K,
         beta,
         res,
-        dense_ncols);
-    TORCH_CHECK(stat == 0, "mkl_sparse_s_mm failed with error code: ", stat);
+        M);
+      TORCH_CHECK(stat == 0, "mkl_sparse_s_mm failed with error code: ", stat);
+    }
   }
 
   inline void sparse_mm(
@@ -128,22 +141,36 @@ class SparseCsrMKLInterface {
       double* dense,
       double alpha,
       double beta,
-      MKL_INT nrows,
-      MKL_INT ncols,
-      MKL_INT dense_ncols) {
-    int stat = mkl_sparse_d_mm(
+      MKL_INT M,
+      MKL_INT K,
+      MKL_INT N) {
+    int stat;
+    if (N == 1) {
+      stat = mkl_sparse_d_mv(
+        SPARSE_OPERATION_NON_TRANSPOSE,
+        alpha,
+        A,
+        desc,
+        dense,
+        beta,
+        res);
+      TORCH_CHECK(stat == 0, "mkl_sparse_d_mv failed with error code: ", stat);
+    }
+    else {
+      stat = mkl_sparse_d_mm(
         SPARSE_OPERATION_NON_TRANSPOSE,
         alpha,
         A,
         desc,
         SPARSE_LAYOUT_ROW_MAJOR,
         dense,
-        dense_ncols,
-        dense_ncols,
+        N,
+        K,
         beta,
         res,
-        dense_ncols);
-    TORCH_CHECK(stat == 0, "mkl_sparse_d_mm failed with error code: ", stat);
+        M);
+      TORCH_CHECK(stat == 0, "mkl_sparse_d_mm failed with error code: ", stat);
+    }
   }
 
   ~SparseCsrMKLInterface() {
@@ -151,6 +178,7 @@ class SparseCsrMKLInterface {
   }
 };
 
+ // res(M, N) = (sparse(M * K) @ dense(K x N))
 template <typename scalar_t>
 static inline void sparse_mm_mkl_template(
     Tensor& res,
