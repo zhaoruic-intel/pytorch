2dbe8f352cdaa304cbbb9f5cc984afe06ed464b3
Ansley Ussery <ansley@fb.com>
2021-05-26 19:34:09 -0700

    Preserve Union type annotations during serialization
    ghstack-source-id: 21206c156f75381f6755cd44ef3a133179aa0b28
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57013

--- test/jit/test_union.py
+++ test/jit/test_union.py
@@ -1,3 +1,4 @@
+import io
 import os
 import sys
 
@@ -343,6 +344,7 @@ class TestUnion(JitTestCase):
 
         buffer = io.BytesIO()
         torch.jit.save(fn, buffer)
+        buffer = io.BytesIO(buffer.getvalue())
         l = torch.jit.load(buffer)
 
         s = l.code
@@ -550,3 +552,22 @@ class TestUnion(JitTestCase):
             return x
 
         self.checkScript(fn, ())
+
+    def test_union_serialization_preserves_type_annotations(self):
+        # This function will fail after being torch.jit.save'd and
+        # torch.jit.load'd if the type annotations aren't preserved
+        # for Union during serialization. We need the `Union[str, int]`
+        # annotation to make sure that `y` is typed as a Union instead
+        # of as a str in one branch and an int in the other
+        def fn(x: int) -> str:
+            if x % 2:
+                y: Union[str, int] = "bar"
+            else:
+                y: Union[str, int] = x
+            if isinstance(y, str):
+                return y
+            else:
+                return "baz"
+
+        self.checkScript(fn, (1,))
+        self.checkScript(fn, (8,))
